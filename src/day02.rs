4990a0ecaff6ceab9ad208cfe81a729fef184d85
denismaxim0v <denis_maxim0v@protonmail.com>
2021-12-02 09:08:01 +0200

    done: day 2 part 1

---
+++ src/day02.rs
@@ -0,0 +1,52 @@
+enum Direction {
+    Up,
+    Down,
+    Forward,
+}
+
+struct Command {
+    direction: Direction,
+    val: i32,
+}
+
+fn parse_command(line: &str) -> Command {
+    let mut split = line.split(" ");
+
+    return Command {
+        direction: parse_direction(split.next().unwrap()),
+        val: split.next().unwrap_or("0").parse::<i32>().unwrap(),
+    };
+}
+
+fn parse_direction(dir: &str) -> Direction {
+    return match dir {
+        "up" => Direction::Up,
+        "down" => Direction::Down,
+        "forward" => Direction::Forward,
+        _ => panic!("failed parsing"),
+    };
+}
+
+pub fn part1(inp: String) {
+
+    let mut commands: Vec<Command> = vec![];
+
+    let raw_commands: Vec<&str> = inp.lines().collect();
+
+    for command in raw_commands {
+        commands.push(parse_command(command));
+    }
+
+    let mut depth = 0;
+    let mut position = 0;
+
+    for command in commands {
+        match command.direction {
+            Direction::Up => depth -= command.val,
+            Direction::Down => depth += command.val,
+            Direction::Forward => position += command.val,
+        }
+    }
+
+    println!("{}", (depth * position));
+}
