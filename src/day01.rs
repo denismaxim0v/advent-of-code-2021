3eef3037668711032a10d7457cf8a927212683d4
denismaxim0v <denis_maxim0v@protonmail.com>
2021-12-02 08:34:42 +0200

    done: day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,29 @@
+fn greater_than_previous(
+    (previous, total): (Option<usize>, usize),
+    current: usize,
+) -> (Option<usize>, usize) {
+    (
+        Some(current),
+        if previous.filter(|&previous| current > previous).is_some() {
+            total + 1
+        } else {
+            total
+        },
+    )
+}
 
 pub fn part1(inp: String) {
-    println!("{}", inp.chars().rev().collect::<String>());
+    let depths = inp
+        .lines()
+        .map(str::parse)
+        .collect::<Result<Vec<usize>, _>>()
+        .unwrap();
+
+    let res = depths
+        .iter()
+        .cloned()
+        .fold((None, 0), greater_than_previous)
+        .1;
+
+    println!("{}", res);
 }
