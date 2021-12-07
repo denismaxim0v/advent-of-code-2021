24bbf6dafa98e56298dae9894c4e03ea009ecef8
denismaxim0v <denis_maxim0v@protonmail.com>
2021-12-07 08:55:37 +0200

    done: day 7 part 2

--- src/day07.rs
+++ src/day07.rs
@@ -8,4 +8,21 @@ pub fn part1(inp: String) {
     let res: isize = input.iter().map(|num| (median - num).abs()).sum();
     println!("{:?}", res);
 }
-pub fn part2(inp: String) {}
+
+pub fn part2(inp: String) {
+    let input: Vec<isize> = inp.trim().split(",").map(|s| s.parse().unwrap()).collect();
+
+    let avg: isize = input.iter().sum::<isize>() / input.len() as isize;
+
+    let calc = |i| {
+        let mut dist = 0;
+        for x in 1..i + 1 {
+            dist = dist + x;
+        }
+        dist
+    };
+
+    let res: isize = input.iter().map(|i| calc((avg - i).abs())).sum();
+
+    println!("{:?}", res);
+}
