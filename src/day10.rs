4cbfbadbfec2aa82389481d0ca502d090fa493bb
denismaxim0v <denis_maxim0v@protonmail.com>
2021-12-10 09:05:13 +0200

    done: day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -61,4 +61,59 @@ pub fn part1(inp: String) {
 
     println!("{}", res);
 }
-pub fn part2(inp: String) {}
+pub fn part2(inp: String) {
+    let mut stack: Vec<&str> = Vec::new();
+    let mut scores: Vec<usize> = Vec::new();
+
+    for line in inp.lines() {
+        let mut removed = false;
+        stack.clear();
+        for c in line.split("").collect::<Vec<&str>>() {
+            match c {
+                "(" | "[" | "{" | "<" => stack.push(c),
+                ")" => {
+                    if stack.pop().unwrap() != "(" {
+                        removed = true;
+                        break;
+                    }
+                }
+                "]" => {
+                    if stack.pop().unwrap() != "[" {
+                        removed = true;
+                        break;
+                    }
+                }
+                "}" => {
+                    if stack.pop().unwrap() != "{" {
+                        removed = true;
+                        break;
+                    }
+                }
+                ">" => {
+                    if stack.pop().unwrap() != "<" {
+                        removed = true;
+                        break;
+                    }
+                }
+                _ => continue,
+            }
+        }
+
+        if !removed {
+            let mut score = 0;
+            for c in stack.iter().rev() {
+                score *= 5;
+                match *c {
+                    "(" => score += 1,
+                    "[" => score += 2,
+                    "{" => score += 3,
+                    "<" => score += 4,
+                    _ => continue,
+                }
+            }
+            scores.push(score);
+        }
+    }
+    scores.sort();
+    println!("{}", scores[scores.len() >> 1]);
+}
